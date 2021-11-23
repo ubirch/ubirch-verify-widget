37ec91ac667785074225e0421b1c77201442af16
dieAte <beate.fiss@indarium.de>
2021-11-23 12:17:38 +0100

    UC-47: update webpack version and fix vulnerabilities

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = function (env) {
   const STAGE = env.STAGE || 'dev';
@@ -20,7 +18,7 @@ module.exports = function (env) {
     module: {
       rules: [
         {test: /\.ts?$/, use: 'ts-loader', exclude: /node_modules/},
-        {test: /\.s[ac]ss$/i, use: ['style-loader', 'css-loader', 'sass-loader']},
+        {test: /\.css$/i, use: ['style-loader', 'css-loader']},
         {
           test: /\.(png|jpe?g|gif)$/i,
           use: [
@@ -53,9 +51,12 @@ module.exports = function (env) {
       new CleanWebpackPlugin(),
     ],
     devServer: {
-      contentBase: path.join(__dirname, 'dist'),
+      static: path.join(__dirname, 'dist'),
       compress: true,
       port: 9102
+    },
+    performance: {
+      hints: false,
     }
   }
 };
